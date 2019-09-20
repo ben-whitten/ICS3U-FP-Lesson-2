d59f42858f9aa1723484f7397dd779f003ad6c39
ben-whitten <benjamin.whitten@mths.ca>
2019-09-20 16:47:36 +0000

    added program

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Whitten
+# Created on: Sep 2019
+# This program says "Hello, World!" on the PyBadge
+
+
+def main():
+    print("\n\n\n")  # Adds 3 blank lines
+    print("Hello, World!")
+
+    while True:
+        # the true makes it repeat forever.
+        pass  # pass is a place holder.
+
+
+if __name__ == "__main__":
+    main()
